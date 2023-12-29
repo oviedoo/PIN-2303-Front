3c997ebe8b57fb921b84839be6628919d1174433
Jeremías Oviedo <jeremiasoviedo01@gmail.com>
2023-12-29 16:57:03 -0300

    se creó el footer

---
+++ src/views/Footer/Footer.js
@@ -0,0 +1,20 @@
+function Footer() {
+    return (
+
+        <footer id="footer">
+            <div className="footer-content">
+                <div className="copy">
+                    <p>2024 &copy; Todos los derechos reservados</p>
+                </div>
+
+                <div className="logo">
+                    <h6>Max And Jeremías</h6>
+                </div>
+            </div>
+
+        </footer>
+
+    )
+}
+
+export default Footer;
