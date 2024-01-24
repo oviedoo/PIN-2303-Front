afe6859bad379f7f6a86e90142faae14c343d4b4
Jeremías Oviedo <jeremiasoviedo01@gmail.com>
2024-01-24 17:57:47 -0300

    se corrigió el form

--- src/views/Message/components/Form.js
+++ src/views/Message/components/Form.js
@@ -2,72 +2,83 @@
 function Form({ handleChange, handleSubmit }) {
 
     return (
-
-
         <section id="contact">
-            <form onSubmit={handleSubmit} >
 
-                <div class="mb-3">
-                    <label for="name" class="form-label">
-                        Nombre:
-                    </label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="name"
-                        onChange={handleChange}
-                        required
-                        maxLength={15}
-                    />
-                    <div id="nameHelp" class="form-text">
-                        Maximo de 15 caracteres.
+            <div className="contact-content">
+                <div>
+                    <h2 className="contact-title">
+
+                        PONTE EN CONTACTO<br />POR CUALQUIER CONSULTA
+                    </h2>
+                </div>
+                <form onSubmit={handleSubmit} >
+
+                    <div class="mb-3">
+                        <label for="name" class="form-label">
+                            Nombre:
+                        </label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="name"
+                            onChange={handleChange}
+                            required
+                            maxLength={15}
+                        />
+                        <div id="nameHelp" class="form-text">
+                            Maximo de 15 caracteres.
+                        </div>
                     </div>
-                </div>
 
-                <div class="mb-3">
-                    <label for="email" class="form-label">
-                        Correo:
-                    </label>
-                    <input
+                    <div class="mb-3">
+                        <label for="email" class="form-label">
+                            Correo:
+                        </label>
+                        <input
 
-                        type="email"
-                        class="form-control"
-                        id="email"
-                        aria-describedby="emailHelp"
-                        onChange={handleChange}
-                        required
-                    />
-                    <div id="emailHelp" class="form-text">
-                        Nunca compartiremos su correo electrónico con nadie más.
+                            type="email"
+                            class="form-control"
+                            id="email"
+                            aria-describedby="emailHelp"
+                            onChange={handleChange}
+                            required
+                        />
+                        <div id="emailHelp" class="form-text">
+                            Nunca compartiremos su correo electrónico con nadie más.
+                        </div>
                     </div>
-                </div>
 
-                <div class="mb-3">
-                    <label for="cell" class="form-label">
-                        Celular:
-                    </label>
-                    <input
-                        type="number"
-                        class="form-control"
-                        id="cell"
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div class="mb-3">
-                    <textarea
-                        type="text"
-                        class="form-control"
-                        id="message"
-                        placeholder="Escribe tu consulta..."
-                        onChange={handleChange}
-                    />
-                </div>
+                    <div class="mb-3">
+                        <label for="cell" class="form-label">
+                            Celular:
+                        </label>
+                        <input
+                            type="number"
+                            class="form-control"
+                            id="cell"
+                            onChange={handleChange}
+                            required
+                        />
+                        <div id="cellHelp" class="form-text">
+                            (+54)
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <textarea
+                            type="text"
+                            class="form-control"
+                            id="message"
+                            placeholder="Escribe tu consulta..."
+                            onChange={handleChange}
+                        />
+                    </div>
 
-                <button type="submit" class="btn btn-primary">
-                    Submit
-                </button>
-            </form>
+                    <button type="submit" class="btn btn-primary">
+                        Submit
+                    </button>
+                </form>
+            </div>
+
         </section>
     );
 }
